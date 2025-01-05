484a60474dcf50c1550e32b08313e6ace4a4662d
Ber173 <nguyenphuc1629@gmail.com>
2025-01-05 13:12:02 +0700

    newfile

---
+++ khoangu.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+int n;
+
+void input()
+{
+}
+
+int main()
+{
+
+    return 0;
+}
